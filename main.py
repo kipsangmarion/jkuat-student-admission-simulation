d7851c020e239d641bb13756357ca74fa3f6097f
Ceasar Njuguna <ceasarnjuguna@gmail.com>
2023-07-11 19:04:41 +0300

    changes to system out output. added new print statement

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def student_arrival(env, server):
 # Define the student service process
 def student_service(env, student, server):
     arrival_time = env.now
+    print(f"The {student} arrived at minute {round(arrival_time)}")
     with server.request() as request:
         yield request
-        yield env.timeout(random.expovariate(1.0 / 7))
-        print('%s served at %g minutes.' % (student, env.now - arrival_time))
+        yield env.timeout(random.expovariate(1.0 / 7))  # service time of 7 minutes
+        print(f"The {student} spent {round(env.now - arrival_time)} minutes in the system ")
 
 
 # Set up the environment and run the simulation
